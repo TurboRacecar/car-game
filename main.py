ec2b5b9fe90ba019726b290ade9e4896b260d0d2
Hien Nguyen <121696745+TurboRacecar@users.noreply.github.com>
2023-01-09 13:02:10 +1100

    Create main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+import turtle
+import time
+
+
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.setup(height=640, width=1000)
+wn.tracer(0)
+wn.title("Driving test")
+
+car = turtle.Turtle()
+car.color("cyan")
+car.speed(0)
+car.shape("triangle")
+car.penup()
+car.state = "ready"
+car.shapesize(stretch_len=2, stretch_wid=1)
+car.moving = 0
+
+def brakes():
+    if car.moving > -16:
+        car.moving -= 4
+
+
+def forward():
+    car.moving += 2
+
+
+def right():
+    car.right(10)
+
+
+def left():
+    car.left(10)
+
+
+wn.listen()
+wn.onkeypress(forward, "Up")
+wn.onkeypress(left, "Left")
+wn.onkeypress(right, "Right")
+wn.onkeypress(brakes, "Down")
+
+while True:
+    wn.update()
+    car.fd(car.moving)
+    time.sleep(0.04)
